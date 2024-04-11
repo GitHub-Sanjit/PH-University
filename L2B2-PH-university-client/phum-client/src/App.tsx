1930dc34fba7a025e146fcfabca11b6a7b9aa2d1
GitHub-Sanjit <sanjit.est14@gmail.com>
2024-04-11 16:40:34 +0600

    Module 15

--- L2B2-PH-university-client/phum-client/src/App.tsx
+++ L2B2-PH-university-client/phum-client/src/App.tsx
@@ -1,11 +1,16 @@
 import MainLayout from "./components/layout/MainLayout";
+import ProtectedRoute from "./components/layout/ProtectedRoute";
 
 
 function App() {
 
-  return <>
-    <MainLayout></MainLayout>
-  </>;
+  return (
+    <>
+      <ProtectedRoute>
+        <MainLayout></MainLayout>
+      </ProtectedRoute>
+    </>
+  );
 }
 
 export default App;
--- L2B2-PH-university-client/phum-client/src/pages/Login.tsx
+++ L2B2-PH-university-client/phum-client/src/pages/Login.tsx
@@ -1,25 +1,30 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
-    const { register, handleSubmit } = useForm({
-        defaultValues: {
-            userId: 'A-0001',
-            password:'admin123',
-      }
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0001",
+      password: "admin123",
+    },
   });
 
-  const [login, { data, error }] = useLoginMutation();
-  console.log("data=>", data);
-  console.log("error=>", error);
+  const [login, { error }] = useLoginMutation();
 
-  const onSubmit = (data) => {
-      const userInfo = {
-          id: data.userId,
-          password: data.password,
-      };
-      login(userInfo);
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
   };
 
   return (
